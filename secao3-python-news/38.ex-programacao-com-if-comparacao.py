ef54d238a748d4a2188bcbf5b883e3326a33f8c0
robinson <robinpaulista85@gmail.com>
2023-05-15 08:09:48 -0300

    Exercício de fixação com if e comparação

---
+++ secao3-python-news/38.ex-programacao-com-if-comparacao.py
@@ -0,0 +1,9 @@
+primeira = int(input('Primeira nota: '))
+segunda = int(input('Segunda nota: '))
+
+if primeira > segunda:
+    print('A primeira nota é maior que a segunda')
+elif primeira < segunda:
+    print('A segunda nota é maior que a primeira')
+else:
+    print('As notas são iguais')
