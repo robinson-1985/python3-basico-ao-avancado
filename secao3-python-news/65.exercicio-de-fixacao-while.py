6c90ace7ec74151e68a1890a7ecc631ce8365efd
robinson <robinpaulista85@gmail.com>
2023-05-24 18:02:42 -0300

    Exercício de fixação com while em Python

---
+++ secao3-python-news/65.exercicio-de-fixacao-while.py
@@ -0,0 +1,12 @@
+nome = 'Robinson Dias'
+
+contador = 0
+novo_nome = ''
+while contador < len(nome):
+    letra = nome[contador]
+    novo_nome += f'*{letra}'
+    contador += 1
+
+novo_nome += '*'
+
+print(novo_nome)
