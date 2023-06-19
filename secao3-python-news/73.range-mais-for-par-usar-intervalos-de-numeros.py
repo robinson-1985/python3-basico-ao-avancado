05c4547173692c54829d1b71b3a62da8ea7b7dc3
robinson <robinpaulista85@gmail.com>
2023-06-19 20:31:24 -0300

    Utilizando range + for para usar intervalos de números em Python

---
+++ secao3-python-news/73.range-mais-for-par-usar-intervalos-de-numeros.py
@@ -0,0 +1,9 @@
+"""
+For + Range
+range -> range(start, stop, step)
+"""
+numeros = range(0, 100, 8) # range(start, stop, step)
+                #começa em 0, vai até 100, de 8 em 8
+
+for numero in numeros:
+    print(numero)
