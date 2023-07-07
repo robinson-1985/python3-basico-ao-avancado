eb70a4018efb498c118924f77cfbdc9bfbcd27cc
robinson <robinpaulista85@gmail.com>
2023-07-07 09:49:57 -0300

    Tipo tuplas (tuples)

---
+++ secao3-python-news/88.tipo-tuplas.py
@@ -0,0 +1,8 @@
+"""
+Tipo tupla - Uma lista imutável
+"""
+nomes = ('Maria', 'Helena', 'Luiz')
+# nomes = tuple(nomes) - transforma tupla em lista
+# nomes = list(nomes) - transforma lista em tupla
+print(nomes[-1])
+print(nomes)
