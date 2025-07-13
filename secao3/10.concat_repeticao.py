dc8b7e1bb503106d9c09d163b46c4f8a0f9514a2
robinson <robinpaulista85@gmail.com>
2025-07-13 19:32:48 -0300

    study: explore arithmetic operators applied to strings

---
+++ secao3/10.concat_repeticao.py
@@ -0,0 +1,8 @@
+concatenacao = 'Robinson' + ' ' + 'Dias'
+concat_str = 'Robinson' + str(1) + 'Dias'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_robinson = 3 * 'Robinson'
+print(a_dez_vezes)
+print(tres_vezes_robinson)
